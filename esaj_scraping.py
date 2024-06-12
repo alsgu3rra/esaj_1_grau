3915f6683f65e46d412e78e40875817c3ea1e8db
Alan Lima da Silva Guerra <164754267+als-guerra@users.noreply.github.com>
2024-06-12 10:21:09 -0300

    abrindo navegador firefox

--- esaj_scraping.py
+++ esaj_scraping.py
@@ -7,7 +7,21 @@ import time
 
 
 def Main():
-  pass
+  firefox_service = Service(GeckoDriverManager().install())
+
+  firefox_options = Options()
+  
+  firefox_options.headless = True
+
+  driver = webdriver.Firefox(service=firefox_service, options=firefox_options)
+
+  url = "https://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=9A0001VSZ0000&processo.foro=334&processo.numero=1000664-45.2024.8.26.0334"
+
+  driver.get(url)
+
+  time.sleep(3)
+
+  driver.quit()
 
 if __name__ == '__main__':
   Main()
