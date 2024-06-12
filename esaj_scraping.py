15b71fa1553dca60f5ec280697a96c65fb3b6365
Alan Lima da Silva Guerra <164754267+als-guerra@users.noreply.github.com>
2024-06-12 10:19:37 -0300

    função principal

---
+++ esaj_scraping.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
+import time
+
+
+def Main():
+  pass
+
+if __name__ == '__main__':
+  Main()
